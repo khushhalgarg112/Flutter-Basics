e20f29221602cd96862acae69c1cb51b4b9a74ca
Khushhal garg <khusgarg1@gmail.com>
2021-11-20 13:43:00 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    int all = 3000;
+    final man = "I Make it using Flutter";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Enactus Shivaji"),
+      ),
+      body: Center(
+          child: Container(
+            child: Text("Welcome to My App $all \n $man "),
+          ),
+        ),
+      drawer: Drawer(),
+      
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcom to My App"),
-          ),
-          ),
-      ),
+      home: HomePage(),
     );
   }
 }
